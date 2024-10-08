fd92603cc5fcc765a57a0dced28d59f21711023d
jenanouar1234 <jenanouar1234@gmail.com>
2024-10-08 12:23:15 +0100

    pushed

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Hanzar Ziad <https://github.com/Jenanouar55>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
